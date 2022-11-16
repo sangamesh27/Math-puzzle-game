2818ced96f9fa29e5fe2d11779738c9097ad8f28
sangamesh patil <sangamesh8309@gmail.com>
2022-11-16 22:37:25 +0530

    complete

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ class Box{
     }
     getTopBox(){
         if(this.y===0) return null
-        return new Box(this.x+1,this.y)
+        return new Box(this.x,this.y-1)
+    }
+    getRightBox(){
+        if(this.x===3) return null
+        return new Box(this.x + 1,this.y)
     }
     getBottomBox(){
         if(this.y===3) return null
@@ -20,7 +24,7 @@ class Box{
             this.getRightBox(),
             this.getBottomBox(),
             this.getLeftBox(),
-        ].filter((box)=>box !=== null)
+        ].filter((box)=>box !== null)
     }
     getRandomNextdoorBox(){
         const nextdoorBoxes = this.getNextdoorBoxes()
@@ -33,8 +37,8 @@ const swapBoxes = (grid,box1,box2) =>{
     grid[box2.y][box2.x]=temp
 }
 const isSolved = (grid) => {
-    return {
-        grid,[0][0] === 1 &&
+    return (
+        grid[0][0] === 1 &&
         grid[0][1] === 2 &&
         grid[0][2] === 3 &&
         grid[0][3] === 4 &&
@@ -50,7 +54,7 @@ const isSolved = (grid) => {
         grid[3][1] === 14 &&
         grid[3][2] === 15 &&
         grid[3][3] === 0
-    }
+    )
 }
 const getRandomGrid=()=>{
     let grid=[
@@ -59,4 +63,122 @@ const getRandomGrid=()=>{
         [9,10,11,12],
         [13,14,15,0],
     ]
+    let blankBox = new Box(3,3)
+    for(let i = 0;i<1000;i++){
+        const randomNextdoorBox = blankBox.getRandomNextdoorBox()
+        swapBoxes(grid,blankBox,randomNextdoorBox)
+        blankBox = randomNextdoorBox
+    }
+    if(isSolved(grid)) return getRandomGrid()
+    return grid
 }
+class State{
+    constructor(grid,move,time,status){
+        this.grid = grid
+        this.move = move
+        this.time = time
+        this.status = status
+    }
+    static ready(){
+        return new State(
+            [
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+            ],
+            0,
+            0,
+            'ready',
+        )
+    }
+    static start(){
+        return new State(getRandomGrid(),0,0,'playing')
+    }
+}
+class Game{
+    constructor(state){
+        this.state = state
+        this.tickId = null
+        this.tick = this.tick.bind(this)
+        this.render()
+        this.handleClickBox = this.handleClickBox.bind(this)
+    }
+    static ready(){
+        return new Game(State.ready())
+    }
+    tick(){
+        this.setState({time:this.state.time+1})
+    }
+    setState(newState){
+        this.state = {...this.state,...newState}
+        this.render()}
+
+        handleclickBox(box){
+            return function(){
+                const nextdoorBoxes = box.getNextdoorBoxes()
+                const blankBox = nextdoorBoxes.find(
+                    (nextdoorBox)=>this.state.grid[nextdoorBox.y][nextdoorBox.x]===0,
+                )
+                if(blankBox){
+                    const newGrid = [...this.state.grid]
+                    swapBoxes(newGrid,box,blankBox)
+                    if(isSolved(newGrid)){
+                        clearInterval(this.tickId)
+                        this.setState({
+                            status:'won',
+                            grid:newGrid,
+                            move: this.state.move + 1,
+                        })
+                    } else{
+                        this.setState({
+                            grid:newGrid,
+                            move:this.state.move +1,
+                        })
+                    }
+                }
+            }.bind(this)
+        }
+        render(){
+            const{grid,move,time,status} = this.state
+            const newGrid = document.createElement('div')
+            newGrid.className = 'grid'
+            for(let i=0;i<4;i++){
+                for(let j = 0;i<4;j++){
+                    const button = document.createElement('button')
+
+                    if(status === 'playing'){
+                        button.addEventListener(
+                            'click',
+                            this.handleClickBox(new Box(j,i)),
+                        )
+                    }
+                    button.textContent = grid[i][j] === 0 ?'':grid[i][j].toString()
+                    newGrid.appendChild(button)
+                }
+            }
+            document.querySelector('.grid').replaceWith(newGrid)
+            
+            const newButton = document.createElement('button')
+            if(status === 'ready') newButton.textContent = 'play'
+            if(status === 'playing') newButton.textContent = 'Reset'
+            if(status === 'won') newButton.textContent = 'play'
+            newButton.addEventListener('click',()=>{
+                clearInterval(this.tickId)
+                this.tickId = setInterval(this.tick,1000)
+                this.setState(State.start())
+            })
+            document.querySelector('.footer button').replaceWith(newButton)
+            //render move
+            document.getElementById(move).textContent = 'Move:$(move)'
+            //render time
+            document.getElementById(time).textContent = 'Time:$(time)'
+            //render message
+            if(status === 'won'){
+                document.querySelector('.message').textContent = 'You win'
+            }else{
+                document.querySelector('.message').textContent = ''
+            }
+        }
+    }
+    const GAME = Game.ready()
